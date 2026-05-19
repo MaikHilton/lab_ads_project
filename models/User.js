f3e2dd3d36b862556d5d13d41e20636d9ef6fe03
IvanYatsenko <yatsenko.ivan@student.sumdu.edu.ua>
2026-05-19 11:22:32 +0300

    refactor: implement user pre-save hook for automatic password hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,6 +30,13 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+// Автоматичне хешування пароля перед збереженням
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
