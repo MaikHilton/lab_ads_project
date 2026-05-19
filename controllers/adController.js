820ed1d9e48a724b703fadeb841c10fae3fbae0a
IvanYatsenko <yatsenko.ivan@student.sumdu.edu.ua>
2026-05-19 11:23:12 +0300

    refactor: simplify controllers by offloading logic to services

--- controllers/adController.js
+++ controllers/adController.js
@@ -1,34 +1,27 @@
-const Ad = require('../models/Ad');
-const AppError = require('../utils/AppError');
 const asyncHandler = require('../middlewares/asyncHandler');
+const adService = require('../services/adService');
 
-exports.getAllAds = asyncHandler(async (req, res, next) => {
-  const ads = await Ad.find().populate('createdBy', 'name email');
+exports.getAllAds = asyncHandler(async (req, res) => {
+  const ads = await adService.getAllAds();
   res.status(200).json({ success: true, count: ads.length, data: ads });
 });
 
-exports.getAd = asyncHandler(async (req, res, next) => {
-  const ad = await Ad.findById(req.params.id).populate('createdBy', 'name email');
-  if (!ad) return next(new AppError('Оголошення не знайдено', 404));
+exports.getAd = asyncHandler(async (req, res) => {
+  const ad = await adService.getAdById(req.params.id);
   res.status(200).json({ success: true, data: ad });
 });
 
-exports.createAd = asyncHandler(async (req, res, next) => {
-  const ad = await Ad.create({
-    ...req.body,
-    createdBy: req.user._id
-  });
+exports.createAd = asyncHandler(async (req, res) => {
+  const ad = await adService.createAd(req.body, req.user._id);
   res.status(201).json({ success: true, data: ad });
 });
 
-exports.updateAd = asyncHandler(async (req, res, next) => {
-  const ad = await Ad.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-  if (!ad) return next(new AppError('Оголошення не знайдено', 404));
+exports.updateAd = asyncHandler(async (req, res) => {
+  const ad = await adService.updateAd(req.params.id, req.body, req.user);
   res.status(200).json({ success: true, data: ad });
 });
 
-exports.deleteAd = asyncHandler(async (req, res, next) => {
-  const ad = await Ad.findByIdAndDelete(req.params.id);
-  if (!ad) return next(new AppError('Оголошення не знайдено', 404));
-  res.status(200).json({ success: true, message: 'Оголошення видалено' });
+exports.deleteAd = asyncHandler(async (req, res) => {
+  await adService.deleteAd(req.params.id);
+  res.status(200).json({ success: true, message: 'Оговорення видалено' });
 });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,47 +1,25 @@
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const AppError = require('../utils/AppError');
 const asyncHandler = require('../middlewares/asyncHandler');
+const authService = require('../services/authService');
 
-const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
+const generateToken = (id, role) =>
+  jwt.sign({ id, role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
   });
-};
 
-exports.register = asyncHandler(async (req, res, next) => {
-  const { name, email, password, confirmPassword } = req.body;
-
-  if (!name || !email || !password || !confirmPassword) {
-    return next(new AppError('All fields are required', 400));
-  }
-  if (password !== confirmPassword) {
-    return next(new AppError('Паролі не збігаються', 400));
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({ name, email, password: hashedPassword });
+exports.register = asyncHandler(async (req, res) => {
+  const user = await authService.registerUser(req.body);
+  const token = generateToken(user._id, user.role);
 
   res.status(201).json({
     success: true,
-    message: 'Registration successful',
+    token,
     user: { id: user._id, name: user.name, email: user.email, role: user.role }
   });
 });
 
-exports.login = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return next(new AppError('Введіть email та пароль', 400));
-  }
-
-  const user = await User.findOne({ email }).select('+password');
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return next(new AppError('Невірний email або пароль', 401));
-  }
-
+exports.login = asyncHandler(async (req, res) => {
+  const user = await authService.loginUser(req.body);
   const token = generateToken(user._id, user.role);
 
   res.status(200).json({
@@ -51,7 +29,6 @@ exports.login = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  res.status(200).json({ success: true, user });
+exports.getMe = asyncHandler(async (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
 });
